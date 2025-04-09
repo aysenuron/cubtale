ee347e31336219c945c4aa8cdf55d998b301a9ee
aysenuron <onaranays@gmail.com>
2025-04-09 11:44:18 +0300

    Add lottie animations package.

---
+++ src/components/Baby.jsx
@@ -0,0 +1,7 @@
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+export default function Baby() {
+  return (
+    <DotLottieReact className="w-80" src="/animation-baby.json" loop autoplay />
+  );
+}
